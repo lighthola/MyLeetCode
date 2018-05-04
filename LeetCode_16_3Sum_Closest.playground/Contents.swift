4045bea5ef0713311a3b85fea69cd327e6221025
Bevis Chen <bevis_chen@soft-world.com.tw>
2018-05-04 17:56:48 +0800

    15. 3Sum: OK

---
+++ LeetCode_16_3Sum_Closest.playground/Contents.swift
@@ -0,0 +1,19 @@
+/*
+ LeetCode - Medium
+ 16. 3Sum Closest
+ 
+ runtime:  ms ms,  % beats of swift submissions
+ inspire:
+ */
+
+import Foundation
+
+class Solution {
+    func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
+        
+        return 0
+    }
+}
+
+
+Solution().threeSumClosest([-1, 2, 1, -4], 1)
