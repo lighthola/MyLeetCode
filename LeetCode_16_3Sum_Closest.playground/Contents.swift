e5d7daa5ead734c24de9e6dbb4817dfd89343e8f
Bevis Chen <bevis_chen@soft-world.com.tw>
2018-05-07 17:10:05 +0800

    16. 3Sum Closest: OK

--- LeetCode_16_3Sum_Closest.playground/Contents.swift
+++ LeetCode_16_3Sum_Closest.playground/Contents.swift
@@ -2,7 +2,7 @@
  LeetCode - Medium
  16. 3Sum Closest
  
- runtime:  ms ms,  % beats of swift submissions
+ runtime: 84 ms, 33.33 % beats of swift submissions
  inspire:
  */
 
@@ -10,10 +10,35 @@ import Foundation
 
 class Solution {
     func threeSumClosest(_ nums: [Int], _ target: Int) -> Int {
+        if nums.count <= 3 {
+            return nums.reduce(into: 0, { (result, num) in
+                result += num
+            })
+        }
         
-        return 0
+        let nums = nums.sorted()
+        var minSum = Int.max - 99999
+        for i in 1..<nums.count-1 {
+            var l = i - 1, r = i + 1
+            while l >= 0 && r < nums.count {
+                let sum = nums[l] + nums[i] + nums[r]
+                if sum == target {
+                    return target
+                } else if sum > target {
+                    l -= 1
+                } else {
+                    r += 1
+                }
+                
+                if abs(sum - target) < abs(minSum - target) {
+                    minSum = sum
+                }
+            }
+        }
+        
+        return minSum
     }
 }
 
 
-Solution().threeSumClosest([-1, 2, 1, -4], 1)
+Solution().threeSumClosest([-3,0,1,2],1)
