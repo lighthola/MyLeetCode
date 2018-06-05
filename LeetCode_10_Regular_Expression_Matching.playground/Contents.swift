4705b08529ac6f57b33e22fb4f2d120bcaa2ce75
Bevis Chen <bevis_chen@soft-world.com.tw>
2018-06-05 10:19:11 +0800

    10. Regular Expression Matching: OK

--- LeetCode_10_Regular_Expression_Matching.playground/Contents.swift
+++ LeetCode_10_Regular_Expression_Matching.playground/Contents.swift
@@ -2,7 +2,7 @@
  LeetCode - Hard
  10. Regular Expression Matching
  
- runtime:  ms,  % beats of swift submissions
+ runtime: 24 ms, 100 % beats of swift submissions
  better ans:
  */
 
@@ -11,37 +11,46 @@ import Foundation
 class Solution {
     func isMatch(_ s: String, _ p: String) -> Bool {
         let s = Array(s), p = Array(p)
+        var rec: [[Bool]] = Array(repeating: Array(repeating: false, count: p.count + 1), count: s.count + 1)
         
-        let m: Int = s.count // 1
-        let n: Int = p.count // 21
-        var f: [[Bool]] = Array<Array<Bool>>(repeating: Array<Bool>(repeating: false, count: n + 1), count: m + 1)
-        f[0][0] = true
-        for i in 1 ... m {
-            f[i][0] = false
+        rec[0][0] = true
+        for i in 0..<p.count {
+            if p[i] == "*" {
+                rec[0][i + 1] = rec[0][i - 1]
+            }
         }
-        for i in 1 ... n {
-            f[0][i] = i > 1 && "*" == p[i-1] && f[0][i-2]
-        }
-        for i in 1 ... m {
-            for j in 1 ... n {
-                if p[j-1] != "*" {
-                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || "." == p[j - 1])
+        
+        for i in 0..<s.count {
+            for j in 0..<p.count {
+                if p[j] != "*" {
+                    if rec[i][j] {
+                        if p[j] == "." || p[j] == s[i] {
+                            rec[i + 1][j + 1] = true
+                        }
+                    }
                 } else {
-                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || "." == p[j - 2]) && f[i - 1][j]
+                    if rec[i + 1][j - 1] {
+                        rec[i + 1][j + 1] = true
+                    } else if rec[i][j - 1] || rec[i][j + 1] {
+                        if p[j - 1] == s[i] || p[j - 1] == "."   {
+                            rec[i + 1][j + 1] = true
+                        }
+                    }
                 }
             }
         }
-        return f[m][n]
+
+        return rec[s.count][p.count]
     }
 }
 
 
 let s = Solution()
-s.isMatch("a", "aa")
-s.isMatch("aa", "a*")
-s.isMatch("ab", ".*c")
-s.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c")
-s.isMatch("ac", "c*a*a*a*a*a*a*a*a*a*c")
+//s.isMatch("ssi", "s*")
+//s.isMatch("aa", "a*")
+//s.isMatch("iss", "is*")
+//s.isMatch("ssipp","s*p*")
+s.isMatch("aaa", "ab*a*c*a")
 
 
 
